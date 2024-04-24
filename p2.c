8350075d93170aa909502b55122882df3d391099
Vidya <152618756+Vidyasank2003@users.noreply.github.com>
2024-04-24 22:02:47 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a number\n");
+    if(a%97==0)
+    printf("The number %d is divisible by 97\n",a);
+    else
+     printf("The number %d is  not divisible by 97\n",a);
+    return 0;
+
+
+
+}
